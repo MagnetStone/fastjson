07c06c05be7450ed7c1d79b0cbfedbfcc182ee92
shangzonghai <shangzonghai@youzan.com>
2018-01-29 00:16:09 +0800

    分析集合序列化实现

--- src/main/java/com/alibaba/fastjson/serializer/CollectionCodec.java
+++ src/main/java/com/alibaba/fastjson/serializer/CollectionCodec.java
@@ -52,6 +52,7 @@ public class CollectionCodec implements ObjectSerializer, ObjectDeserializer {
         if (out.isEnabled(SerializerFeature.WriteClassName)
                 || SerializerFeature.isEnabled(features, SerializerFeature.WriteClassName))
         {
+            /** 获取字段泛型类型 */
             elementType = TypeUtils.getCollectionItemType(fieldType);
         }
 
@@ -84,11 +85,13 @@ public class CollectionCodec implements ObjectSerializer, ObjectDeserializer {
 
                 Class<?> clazz = item.getClass();
 
+                /** 获取整形类型值，输出 */
                 if (clazz == Integer.class) {
                     out.writeInt(((Integer) item).intValue());
                     continue;
                 }
 
+                /** 获取整形长类型值，输出并添加L标识(如果开启WriteClassName特性) */
                 if (clazz == Long.class) {
                     out.writeLong(((Long) item).longValue());
 
@@ -98,6 +101,7 @@ public class CollectionCodec implements ObjectSerializer, ObjectDeserializer {
                     continue;
                 }
 
+                /** 根据集合类型查找序列化实例处理，JavaBeanSerializer后面单独分析 */
                 ObjectSerializer itemSerializer = serializer.getObjectWriter(clazz);
                 if (SerializerFeature.isEnabled(features, SerializerFeature.WriteClassName)
                         && itemSerializer instanceof JavaBeanSerializer) {
